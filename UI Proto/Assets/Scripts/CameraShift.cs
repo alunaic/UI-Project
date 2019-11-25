2e8e7d950d020f0e0948fa40e31d05b09d55962d
AlinaC <56270941+alunaic@users.noreply.github.com>
2019-11-25 01:16:26 -0500

    Added basic script setup for choice branch and camera shift. Calling from one script to the next needs fixing. Scripts need assigning to the correct objects Text/titles needs to be implemented

---
+++ UI Proto/Assets/Scripts/CameraShift.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShift : MonoBehaviour
+{   public Camera MainCam;
+    public Camera TopCam;
+    public Camera DownCam;
+
+    void Start()
+    {
+        MainCam.enabled = true;
+        TopCam.enabled = false;
+        DownCam.enabled = false;
+
+    }
+
+    void Update()
+    {
+        if (IsQuestA == true)
+        {   
+            DownCam.enabled = true;
+            TopCam.enabled = false;
+            MainCam.enabled = false;
+            
+        }
+        else
+        {
+            MainCam.enabled = true;
+            TopCam.enabled = false;
+            DownCam.enabled = false;
+        }
+
+        if (IsQuestB == true)
+        {
+            TopCam.enabled = true;
+            MainCam.enabled = false;
+            DownCam.enabled = false;
+        }
+        else
+        {
+            MainCam.enabled = true;
+            TopCam.enabled = false;
+            DownCam.enabled = false;
+        }
+
+    }
+}
